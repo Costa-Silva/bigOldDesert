da8c3151948f9cb054a6eef455718bd0847f51ea
paulo <pjs.figueira@hotmail.com>
2016-03-26 17:55:06 +0000

    input reading and output writing

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            int numberOfProb = Integer.parseInt(in.readLine());
+
+            for(int i = 0; i<numberOfProb;i++){
+                String problem = in.readLine();
+                DesertProblem desertProblem = new DesertProblem(problem.toCharArray());
+                System.out.println(Arrays.toString(desertProblem.getMinimumCost()));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
