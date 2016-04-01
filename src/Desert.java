bf9d1036950141d48e0931f1b75c479244748eb1
paulo <pjs.figueira@hotmail.com>
2016-04-01 20:39:24 +0100

    Final Mooshak version

--- src/Desert.java
+++ src/Desert.java
@@ -1,8 +1,6 @@
-import java.util.HashMap;
-import java.util.Map;
 
 /**
- * Created by Antonio on 30/03/16.
+ * Created by Antonio Silva 43381 && Paulo Figueira 43268
  */
 public class Desert {
 
@@ -19,252 +17,92 @@ public class Desert {
     private static final char LAKE = 'L';
     private static final char PIT = 'P';
     private static final char MOUNTAIN = 'M';
+    private static final int BALLONPOS = 1;
+    private static final int CANOEPOS = 2;
+    private static final int PLANKPOS = 3;
+    private static final int PLANPOS = 0;
     private char[] path;
     private long matrixPath[][];
-
-
+    private long lowestValue;
     private long previousValuePlain;
     private long previousValueBalloon;
     private long previousValueCanoe;
     private long previousValuePlank;
 
     public Desert(char[] path){
-
         this.path=path;
-
-
+        lowestValue=0;
         previousValuePlain = Integer.MAX_VALUE;
         previousValueBalloon = Integer.MAX_VALUE;
         previousValueCanoe = Integer.MAX_VALUE;
         previousValuePlank = Integer.MAX_VALUE;
-
-
         matrixPath = new long[4][path.length];
-
     }
 
-
-
     public long problem(){
-
         fillFirst(path[0],0);
-
-        long lowestValue;
-        long valueAux;
         for (int i =1; i<path.length;i++){
-
              previousValuePlain = matrixPath[0][i-1];
              previousValueBalloon = matrixPath[1][i-1];
              previousValueCanoe = matrixPath[2][i-1];
              previousValuePlank = matrixPath[3][i-1];
-
-
             lowestValue = Math.min(Math.min(Math.min(previousValueBalloon,previousValuePlank),previousValueCanoe),previousValuePlain);
-
             char terrain = path[i];
-
             switch (terrain){
-
                 case PLAIN:
-
-                    if (previousValuePlain!= Integer.MAX_VALUE) {
-                        matrixPath[0][i] = previousValuePlain+ DEFAULTCOST;
-                    }else{
-                        matrixPath[0][i] = lowestValue + DEFAULTCOST+LEAVING;
-                    }
-
-
-                    if (previousValueCanoe!=Integer.MAX_VALUE){
-                        matrixPath[2][i] = previousValueCanoe + DEFAULTCOST + ENTERING + LEAVING;
-                    }else{
-                        matrixPath[2][i] = Integer.MAX_VALUE;
-                    }
-
-
-                    if (previousValueBalloon != Integer.MAX_VALUE) {
-
-                        matrixPath[1][i] = previousValueBalloon + ENTERING+LEAVING+DEFAULTCOST;
-                    }else{
-                        matrixPath[1][i] = Integer.MAX_VALUE;
-                    }
-
-                    if (previousValuePlank!=Integer.MAX_VALUE){
-                        matrixPath[3][i] = previousValuePlank + DEFAULTCOST + ENTERING + LEAVING;
-                    }else{
-                        matrixPath[3][i] = Integer.MAX_VALUE;
-                    }
-
+                    treatPlain(true,PLANPOS,i,previousValuePlain);
+                    treatPlain(false,CANOEPOS,i,previousValueCanoe);
+                    treatPlain(false,BALLONPOS,i,previousValueBalloon);
+                    treatPlain(false,PLANKPOS,i,previousValuePlank);
                     break;
-
                 case BALLOON:
-                    if (previousValuePlain!= Integer.MAX_VALUE) {
-                        matrixPath[0][i] = previousValuePlain+ DEFAULTCOST;
-                    }else{
-                        matrixPath[0][i] = lowestValue + ENTERING + DEFAULTCOST;
-                    }
-
-
-                    if (previousValueCanoe!=Integer.MAX_VALUE){
-                        matrixPath[2][i] = previousValueCanoe + DEFAULTCOST + ENTERING + LEAVING;
-                    }else{
-                        matrixPath[2][i] = Integer.MAX_VALUE;
-                    }
-
-
-                    if (previousValueBalloon != Integer.MAX_VALUE) {
-                        matrixPath[1][i] = getBestChoice(lowestValue,previousValueBalloon,i);
-                    }else{
-                        if (isObstacle(path[i-1]))
-                            matrixPath[1][i] = lowestValue + ENTERING+DEFAULTCOST+LEAVING;
-                        else matrixPath[1][i] = lowestValue + DEFAULTCOST+LEAVING;
-                    }
-
-                    if (previousValuePlank!=Integer.MAX_VALUE){
-                        matrixPath[3][i] = previousValuePlank + DEFAULTCOST + ENTERING + LEAVING;
-                    }else{
-                        matrixPath[3][i] = Integer.MAX_VALUE;
-                    }
-
+                    treatPlain(true,BALLONPOS,i,previousValueBalloon);
+                    treatObject(BALLONPOS,CANOEPOS,i,previousValueCanoe);
+                    treatObject(BALLONPOS,BALLONPOS,i,previousValueBalloon);
+                    treatObject(BALLONPOS,PLANKPOS,i,previousValuePlank);
                     break;
-
                 case CANOE:
-                    if (previousValuePlain!= Integer.MAX_VALUE) {
-                        matrixPath[0][i] = previousValuePlain+ DEFAULTCOST;
-                    }else{
-                        matrixPath[0][i] = lowestValue + ENTERING + DEFAULTCOST;
-                    }
-
-
-                    if (previousValueCanoe!=Integer.MAX_VALUE){
-                        matrixPath[2][i] = getBestChoice(lowestValue,previousValueCanoe,i);
-                    }else{
-                        if (isObstacle(path[i-1]))
-                            matrixPath[2][i] = lowestValue + ENTERING+DEFAULTCOST+LEAVING;
-                        else matrixPath[2][i] = lowestValue + DEFAULTCOST+LEAVING;
-
-                    }
-
-
-                    if (previousValueBalloon != Integer.MAX_VALUE) {
-
-                        matrixPath[1][i] = previousValueBalloon + ENTERING+LEAVING+DEFAULTCOST;
-                    }else{
-                        matrixPath[1][i] = Integer.MAX_VALUE;
-                    }
-
-                    if (previousValuePlank!=Integer.MAX_VALUE){
-                        matrixPath[3][i] = previousValuePlank + DEFAULTCOST + ENTERING + LEAVING;
-                    }else{
-                        matrixPath[3][i] = Integer.MAX_VALUE;
-                    }
-
+                    treatPlain(true,CANOEPOS,i,previousValueCanoe);
+                    treatObject(CANOEPOS,CANOEPOS,i,previousValueCanoe);
+                    treatObject(CANOEPOS,BALLONPOS,i,previousValueBalloon);
+                    treatObject(CANOEPOS,PLANKPOS,i,previousValuePlank);
                     break;
                 case PLANK:
-                    if (previousValuePlain!= Integer.MAX_VALUE) {
-                        matrixPath[0][i] = previousValuePlain+ DEFAULTCOST;
-                    }else{
-                        matrixPath[0][i] = lowestValue + ENTERING + DEFAULTCOST;
-                    }
-
-
-                    if (previousValueCanoe!=Integer.MAX_VALUE){
-                        matrixPath[2][i] = previousValueCanoe + DEFAULTCOST + ENTERING + LEAVING;
-                    }else{
-                        matrixPath[2][i] = Integer.MAX_VALUE;
-                    }
-
-
-                    if (previousValueBalloon != Integer.MAX_VALUE) {
-
-                        matrixPath[1][i] = previousValueBalloon + ENTERING+LEAVING+DEFAULTCOST;
-                    }else{
-                        matrixPath[1][i] = Integer.MAX_VALUE;
-                    }
-
-                    if (previousValuePlank!=Integer.MAX_VALUE){
-                        matrixPath[3][i] = getBestChoice(lowestValue,previousValuePlank,i);
-                    }else{
-                        if (isObstacle(path[i-1]))
-                            matrixPath[3][i] = lowestValue + ENTERING+DEFAULTCOST+LEAVING;
-                        else matrixPath[3][i] = lowestValue + DEFAULTCOST+LEAVING;
-
-                    }
-
+                    treatPlain(true,PLANKPOS,i,previousValuePlank);
+                    treatObject(PLANKPOS,CANOEPOS,i,previousValueCanoe);
+                    treatObject(PLANKPOS,BALLONPOS,i,previousValueBalloon);
+                    treatObject(PLANKPOS,PLANKPOS,i,previousValuePlank);
                     break;
-
                 case MOUNTAIN:
-                    matrixPath[0][i] = Integer.MAX_VALUE;
-                    matrixPath[2][i] = Integer.MAX_VALUE;
-                    matrixPath[3][i] = Integer.MAX_VALUE;
-
-                    if (previousValueBalloon != Integer.MAX_VALUE) {
-
-                        matrixPath[1][i] = previousValueBalloon + BALLOONTIME;
-                    }else{
-                        matrixPath[1][i] = Integer.MAX_VALUE;
-                    }
+                    matrixPath[PLANPOS][i] = Integer.MAX_VALUE;
+                    matrixPath[CANOEPOS][i] = Integer.MAX_VALUE;
+                    matrixPath[PLANKPOS][i] = Integer.MAX_VALUE;
+                    crossObstacle(BALLONPOS,i,previousValueBalloon,BALLOONTIME);
                     break;
-
                 case PIT:
-
-                    matrixPath[0][i] = Integer.MAX_VALUE;
-                    matrixPath[2][i] = Integer.MAX_VALUE;
-
-                    if (previousValueBalloon != Integer.MAX_VALUE) {
-
-                        matrixPath[1][i] = previousValueBalloon + BALLOONTIME;
-                    }else{
-                        matrixPath[1][i] = Integer.MAX_VALUE;
-                    }
-
-                    if (previousValuePlank != Integer.MAX_VALUE) {
-                        matrixPath[3][i]= previousValuePlank+PLANKTIME;
-
-                    }else{
-                        matrixPath[3][i] =Integer.MAX_VALUE;
-                    }
+                    matrixPath[PLANPOS][i] = Integer.MAX_VALUE;
+                    matrixPath[CANOEPOS][i] = Integer.MAX_VALUE;
+                    crossObstacle(BALLONPOS,i,previousValueBalloon,BALLOONTIME);
+                    crossObstacle(PLANKPOS,i,previousValuePlank,PLANKTIME);
                     break;
                 case LAKE:
-
-                    matrixPath[0][i] = Integer.MAX_VALUE;
-
-                    if (previousValueBalloon != Integer.MAX_VALUE) {
-
-                        matrixPath[1][i] = previousValueBalloon + BALLOONTIME;
-                    }else{
-                        matrixPath[1][i] = Integer.MAX_VALUE;
-                    }
-
-                    if (previousValueCanoe!=Integer.MAX_VALUE){
-
-                        matrixPath[2][i] = previousValueCanoe+CANOETIME;
-                    }else{
-                        matrixPath[2][i] = Integer.MAX_VALUE;
-                    }
-
-                    if (previousValuePlank != Integer.MAX_VALUE) {
-                        matrixPath[3][i]= previousValuePlank+PLANKTIME;
-
-                    }else{
-                        matrixPath[3][i] = Integer.MAX_VALUE;
-                    }
+                    matrixPath[PLANPOS][i] = Integer.MAX_VALUE;
+                    crossObstacle(BALLONPOS,i,previousValueBalloon,BALLOONTIME);
+                    crossObstacle(CANOEPOS,i,previousValueCanoe,CANOETIME);
+                    crossObstacle(PLANKPOS,i,previousValuePlank,PLANKTIME);
                     break;
-
             }
-
-
-
-
-
-
         }
-
-        lowestValue = Math.min(Math.min(Math.min(matrixPath[1][path.length-1],matrixPath[2][path.length-1]),matrixPath[3][path.length-1]),matrixPath[0][path.length-1]);
-
-
-        return lowestValue;
+        return Math.min(Math.min(Math.min(matrixPath[BALLONPOS][path.length-1],matrixPath[CANOEPOS][path.length-1]),matrixPath[PLANKPOS][path.length-1]),matrixPath[PLANPOS][path.length-1]);
     }
 
+    private void crossObstacle(int objectpos,int i,long previousValue,int crosstime){
+        if (previousValue != Integer.MAX_VALUE) {
+            matrixPath[objectpos][i]= previousValue+crosstime;
+        }else{
+            matrixPath[objectpos][i] = Integer.MAX_VALUE;
+        }
+    }
     private boolean isObstacle(char obs){
         return (obs == MOUNTAIN || obs == LAKE || obs == PIT);
     }
@@ -273,57 +111,68 @@ public class Desert {
     private long getBestChoice(long lowestValue,long previousValue,int i){
         long value1 = previousValue + ENTERING+LEAVING+DEFAULTCOST;
         long value2 = lowestValue+DEFAULTCOST+LEAVING;
-
         if (isObstacle(path[i-1]))
            value2+=ENTERING;
-
         return Math.min(value1,value2);
     }
 
-    private void fillFirst(char c,int pos) {
-
-
-
-        switch (c){
-
-            case PLAIN:
-                matrixPath[0][pos] = 1;
-
-                matrixPath[1][pos]=Integer.MAX_VALUE;
-
-                matrixPath[2][pos]=Integer.MAX_VALUE;
-
-                matrixPath[3][pos]=Integer.MAX_VALUE;
-                break;
-            case BALLOON:
-                matrixPath[1][pos] = 2;
-
-                matrixPath[0][pos]=1;
-
-                matrixPath[2][pos]=Integer.MAX_VALUE;
-
-                matrixPath[3][pos]=Integer.MAX_VALUE;
-                break;
-            case CANOE:
-                matrixPath[2][pos] = 2;
-
-                matrixPath[0][pos]=1;
-
-                matrixPath[1][pos]=Integer.MAX_VALUE;
-
-                matrixPath[3][pos]=Integer.MAX_VALUE;
-                break;
-            case PLANK:
-                matrixPath[3][pos] = 2;
-
-                matrixPath[0][pos]=1;
-
-                matrixPath[1][pos]=Integer.MAX_VALUE;
-
-                matrixPath[2][pos]=Integer.MAX_VALUE;
-                break;
+    private void treatPlain(boolean exp,int planepos,int i,long previousValue){
+        if(exp) {
+            if (previousValuePlain != Integer.MAX_VALUE) {
+                matrixPath[PLANPOS][i] = previousValuePlain + DEFAULTCOST;
+            } else {
+                matrixPath[PLANPOS][i] = lowestValue + DEFAULTCOST + LEAVING;
+            }
+        }else {
+            if (previousValue != Integer.MAX_VALUE) {
+                matrixPath[planepos][i] = previousValue + DEFAULTCOST + ENTERING + LEAVING;
+            } else {
+                matrixPath[planepos][i] = Integer.MAX_VALUE;
+            }
         }
     }
 
+    private void treatObject(int plane,int planepos,int i,long previousValue){
+        if (plane!=planepos) {
+            if (previousValue != Integer.MAX_VALUE) {
+                matrixPath[planepos][i] = previousValue + ENTERING + LEAVING + DEFAULTCOST;
+            } else {
+                matrixPath[planepos][i] = Integer.MAX_VALUE;
+            }
+        }else {
+            if (previousValue!=Integer.MAX_VALUE){
+                matrixPath[planepos][i] = getBestChoice(lowestValue,previousValue,i);
+            }else{
+                if (isObstacle(path[i-1]))
+                    matrixPath[planepos][i] = lowestValue + ENTERING+DEFAULTCOST+LEAVING;
+                else matrixPath[planepos][i] = lowestValue + DEFAULTCOST+LEAVING;
+            }
+        }
+    }
 
+    private void fillFirst(char c,int pos) {
+        matrixPath[PLANPOS][pos] = DEFAULTCOST;
+        switch (c){
+            case PLAIN:
+                matrixPath[BALLONPOS][pos]=Integer.MAX_VALUE;
+                matrixPath[CANOEPOS][pos]=Integer.MAX_VALUE;
+                matrixPath[PLANKPOS][pos]=Integer.MAX_VALUE;
+                break;
+            case BALLOON:
+                matrixPath[BALLONPOS][pos] = DEFAULTCOST + LEAVING;
+                matrixPath[CANOEPOS][pos]=Integer.MAX_VALUE;
+                matrixPath[PLANKPOS][pos]=Integer.MAX_VALUE;
+                break;
+            case CANOE:
+                matrixPath[BALLONPOS][pos]=Integer.MAX_VALUE;
+                matrixPath[CANOEPOS][pos] = DEFAULTCOST + LEAVING;
+                matrixPath[PLANKPOS][pos]=Integer.MAX_VALUE;
+                break;
+            case PLANK:
+                matrixPath[BALLONPOS][pos]=Integer.MAX_VALUE;
+                matrixPath[CANOEPOS][pos]=Integer.MAX_VALUE;
+                matrixPath[PLANKPOS][pos] = DEFAULTCOST + LEAVING;
+                break;
+        }
+    }
 }
